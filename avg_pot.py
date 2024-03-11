ee4b456cf7e304a24981f369c625d16463a2f19c
Hussam Aloufi <122716453+HAloufi99@users.noreply.github.com>
2024-03-11 10:42:10 +0300

    Add files via upload

--- avg_pot.py
+++ avg_pot.py
@@ -16,8 +16,16 @@ print("--- Reading the input file ---")
 
 LOCPOT = open(input_file, "r")
 LOCPOT_data = LOCPOT.readlines()
-
 LOCPOT_data_column = []
+
+if 0 <= 6 < len(LOCPOT_data):
+    line = LOCPOT_data[6].strip()
+    
+    # Split the line into individual numbers, convert them to integers, and sum them up
+    atoms_coord = sum(map(int, line.split()))
+    
+    # Print the total sum
+    print("Total number of atoms", ":", atoms_coord)
 
 # Iterate over each entry in LOCPOT_data
 for entry in LOCPOT_data:
@@ -38,9 +46,9 @@ a=np.array([LOCPOT_data_column[1],LOCPOT_data_column[2],LOCPOT_data_column[3]])
 b=np.array([LOCPOT_data_column[4],LOCPOT_data_column[5],LOCPOT_data_column[6]])
 c=np.array([LOCPOT_data_column[7],LOCPOT_data_column[8],LOCPOT_data_column[9]])
 
-Nx=int(LOCPOT_data_column[17])
-Ny=int(LOCPOT_data_column[18])
-Nz=int(LOCPOT_data_column[19])
+Nx=int(LOCPOT_data_column[13+atoms_coord*3])
+Ny=int(LOCPOT_data_column[14+atoms_coord*3])
+Nz=int(LOCPOT_data_column[15+atoms_coord*3])
 
 vol = np.abs(np.dot(a,(np.cross(b,c))))
 print("--- The volme of the unit cell is ", vol,' Å')
@@ -48,7 +56,7 @@ plane_values = []
 z_coord = [(Z - 1) / Nz * c[2] for Z in range(1, Nz + 1)]
 
 
-index = 19  # Initialize the index to start from LOCPOT_data_column[20]
+index = 15 + atoms_coord  # Initialize the index to start from LOCPOT_data_column[20]
 
 for Z in range(Nz):
     plane_sum = 0
@@ -57,8 +65,16 @@ for Z in range(Nz):
             index += 1  # Increment the index for each element within the loop
             plane_sum += LOCPOT_data_column[index] 
     # Calculate the average value for the current plane and append it to the list
-    average_value = plane_sum / (Nx * Ny * vol)
+    average_value = plane_sum / (Nx * Ny)
     plane_values.append(average_value)
+
+print("\n Maximum average potential value: ", max(plane_values))
+print("\n Minimum average potential value: ", min(plane_values))
+
+
+#normalize = input("\n Do you want to normalize the potential with the maximum value set to zero? (y/n): ")
+#if normalize.lower() == "y":
+#    plane_values= plane_values - max(plane_values)
 
 print("--- Writing data as an output ---")
 
@@ -67,7 +83,7 @@ with open(output_file, 'w') as file:
     for z, value in zip(z_coord, plane_values):
         file.write(f"{z} {value}\n")
 
-plot_data = input("Do you want to plot the data? (y/n): ")
+plot_data = input("\n Do you want to plot the data? (y/n): ")
 
 # Check if the user wants to plot the data
 if plot_data.lower() == "y":
